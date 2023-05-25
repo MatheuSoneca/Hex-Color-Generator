03977e570014d4062f4760e6ec0a320061be2e39
Matheus Viana Dantas <matheusvianad@hotmail.com>
2023-05-24 21:17:31 -0300

    Small changes in the style

--- script.js
+++ script.js
@@ -4,19 +4,20 @@ const bodyColor = document.querySelector("body")
 
 
 const generateHexColor = () =>{
-  //gerar um numero hex aleatorio
   const hexNumber = Math.floor(Math.random()*16777215).toString(16)
   const color = "#" + hexNumber
   return color
 }
 
 const changeBackgroundColor = () => {
-  // pegar o numero hex aleatorio e aplicar no background (nao faço ideia de como fazer isso mas blz)
   const randomColor = generateHexColor()
-  colorInput.value = bodyColor.style.backgroundColor = randomColor
+  colorInput.value = button.style.color = bodyColor.style.backgroundColor = randomColor
 }
 
 button.onclick = () => changeBackgroundColor()
 colorInput.onmouseout = () => {
-  bodyColor.style.backgroundColor = colorInput.value
+  bodyColor.style.backgroundColor = button.style.color = colorInput.value
 }
+
+/* todo
+  Change the font color for one default, and add some shadow. */
